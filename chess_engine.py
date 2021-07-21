ffd892be54a186e9d9c063f88714ccf2862fd3be
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-07-21 11:50:27 -0500

    Adding Pawn Moves

--- chess_engine.py
+++ chess_engine.py
@@ -1,5 +1,3 @@
-
-
 class GameState():
     def __init__(self):
         # 8x8 Board on a 2d list, each element has 2 characters.
@@ -33,6 +31,36 @@ class GameState():
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.whiteToMove = not self.whiteToMove
 
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    def getAllPossibleMoves(self):
+        moves = [Move((6,4), (4, 4), self.board)]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) and (turn == "b" and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p': 
+                        self.getPawnMoves(r, c, moves)
+
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+
+        return moves
+                    
+'''
+All moves of the pieces...
+'''
+
+def getPawnMoves(self, r, c, moves):
+    pass
+
+def getRockMoves(self, r, c, moves):
+    pass
+
+
+
 class Move():
         ranksToRows = {"1": 7, "2": 6, "3": 5, "4":4,
                     "5": 3, "6": 2, "7": 1, "8": 0}
@@ -51,6 +79,18 @@ class Move():
             self.endCol = endSq[1]
             self.pieceMoved = board[self.startRow][self.startCol]
             self.pieceCaptured = board[self.endRow][self.endCol]
+            self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+            print(self.moveID) 
+        
+        
+        #Overriding the Method
+
+        def __eq__(self, other):
+            if isinstance(other, Move):
+                return self.moveID == other.moveID
+
+            return False
+
 
         def getChessNotation(self):
             return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
--- chess_main.py
+++ chess_main.py
@@ -27,10 +27,14 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = GameState()
+    validMoves = gs.getAllPossibleMoves()
+    moveMade = False
     loadImages()
     running = True
     sqSelected = ()
     playerClicks = []
+
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -49,12 +53,18 @@ def main():
                 if len(playerClicks) == 2:
                     move = Move(playerClicks[0],playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()
                     playerClicks = []
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     gs.undoMove()
+                    moveMade = True
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
