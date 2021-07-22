621896e41cddbfd0872c210a5e65f4ace1e03a9a
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-07-22 12:04:16 -0500

    Adding Moves all pieces

--- chess_engine.py
+++ chess_engine.py
@@ -37,17 +37,74 @@ class GameState():
     def getValidMoves(self):
         return self.getAllPossibleMoves()
 
-    def getKnightMoves(self, r, c, moves):
-        pass
-    def getBishopMoves(self, r, c, moves):
-        pass
     def getQueenMoves(self, r, c, moves):
-        pass
-    def getKingMoves(self, r, c, moves):
-        pass
-    def getRockMoves(self, r, c, moves):
-        pass
+        self.getRockMoves(r, c, moves)
+        self.getBishopMoves(r, c, moves)
 
+    def getKnightMoves(self, r, c, moves):
+        knightMoves = ((-2,-1), (-2,1),(-1,2), (1,-2), (1,2),(2,-1),(2,1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r,c),(endRow, endCol), self.board))
+
+    def getBishopMoves(self, r, c, moves):
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemyColor = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1,8):
+                endRow = r + d[0] * i
+                endCol = c+ d[1]* i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
+    
+
+
+    def getKingMoves(self, r, c, moves):
+        kingMoves = ((-1, -1), (-1, 0), (-1, 1), (0,-1), (0, 1), (1,-1),(1,0),(1,1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            endRow = r + kingMoves[i][0]
+            endCol = c + kingMoves[i][1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r,c), (endRow, endCol), self.board)) 
+
+
+
+    def getRockMoves(self, r, c, moves):
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1))
+        enemyColor = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1,8):
+                endRow = r + d[0] * i
+                endCol = c+ d[1]* i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
+                    
 
     def getAllPossibleMoves(self):
         moves = []
@@ -76,7 +133,19 @@ class GameState():
                     moves.append(Move((r,c), (r-1,c+1),self.board))
 
         else:
-            pass
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r, c), (r+1, c), self.board))
+                if r == 1 and self.board[r+2][c] == "--":
+                    moves.append(Move((r,c), (r+2,c),self.board))
+
+            if c-1 >= 0:
+                if self.board[r+1][c-1][0] == 'w': #Enemy piece to capture
+                    moves.append(Move((r,c), (r+1,c-1),self.board))
+            
+            if c+1 <= 7:
+                if self.board[r+1][c+1][0] == 'w': 
+                    moves.append(Move((r,c), (r+1,c+1),self.board))
+
 
 
 
